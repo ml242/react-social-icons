332b92c99a9d080662b70c1b701d3658e9d74fc9
matt <matt.lucas@mindgeek.com>
2018-08-13 12:49:30 -0400

    Revert "Update webpack.config.js"
    This reverts commit 2b984564e3ad8fa70a4fb0108a2832e4619ae1b5.

--- webpack.config.js
+++ webpack.config.js
@@ -1,59 +1,59 @@
-// const fs = require('fs');
-// const path = require('path');
-// const webpack = require('webpack');
+const fs = require('fs');
+const path = require('path');
+const webpack = require('webpack');
 
-// const EXAMPLES_DIR = path.resolve(__dirname, 'examples');
+const EXAMPLES_DIR = path.resolve(__dirname, 'examples');
 
-// function isDirectory(dir) {
-//   return fs.lstatSync(dir).isDirectory();
-// }
+function isDirectory(dir) {
+  return fs.lstatSync(dir).isDirectory();
+}
 
-// function buildEntries() {
-//   return fs.readdirSync(EXAMPLES_DIR).reduce((entries, dir) => {
-//     if (dir === 'build') {
-//       return entries;
-//     }
+function buildEntries() {
+  return fs.readdirSync(EXAMPLES_DIR).reduce((entries, dir) => {
+    if (dir === 'build') {
+      return entries;
+    }
 
-//     const isDraft = dir.charAt(0) === '_';
+    const isDraft = dir.charAt(0) === '_';
 
-//     if (!isDraft && isDirectory(path.join(EXAMPLES_DIR, dir))) {
-//       entries[dir] = path.join(EXAMPLES_DIR, dir, 'app.js'); // eslint-disable-line
-//     }
+    if (!isDraft && isDirectory(path.join(EXAMPLES_DIR, dir))) {
+      entries[dir] = path.join(EXAMPLES_DIR, dir, 'app.js'); // eslint-disable-line
+    }
 
-//     return entries;
-//   }, {});
-// }
+    return entries;
+  }, {});
+}
 
-// module.exports = {
+module.exports = {
 
-//   entry: buildEntries(),
+  entry: buildEntries(),
 
-//   output: {
-//     filename: '[name].js',
-//     chunkFilename: '[id].chunk.js',
-//     path: 'examples/__build__',
-//     publicPath: '/__build__/',
-//   },
+  output: {
+    filename: '[name].js',
+    chunkFilename: '[id].chunk.js',
+    path: 'examples/__build__',
+    publicPath: '/__build__/',
+  },
 
-//   module: {
-//     loaders: [
-//       {
-//         test: /\.js$/,
-//         loader: 'babel',
-//         exclude: path.resolve(__dirname, 'node_modules'),
-//         query: {
-//           presets: ['es2015-loose', 'stage-0', 'react', 'react-optimize'],
-//           plugins: ['transform-runtime'],
-//         },
-//       },
-//       {
-//         test: /\.json$/,
-//         loader: 'json',
-//       },
-//     ],
-//   },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel',
+        exclude: path.resolve(__dirname, 'node_modules'),
+        query: {
+          presets: ['es2015-loose', 'stage-0', 'react', 'react-optimize'],
+          plugins: ['transform-runtime'],
+        },
+      },
+      {
+        test: /\.json$/,
+        loader: 'json',
+      },
+    ],
+  },
 
-//   plugins: [
-//     new webpack.optimize.CommonsChunkPlugin('shared.js'),
-//   ],
-// };
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin('shared.js'),
+  ],
+};
